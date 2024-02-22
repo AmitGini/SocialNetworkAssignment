41adf390d1149aba89ce285f70083247f12ecb13
Amit Gini <Amitgini0369@gmail.com>
2024-02-22 20:34:57 +0200

    update

--- PostSale.py
+++ PostSale.py
@@ -2,35 +2,30 @@ from CustomErrors import SalePostCreationError, WrongPassword, ProductSoldError,
 from Post import Post
 
 # Factory Design Pattern
-# Constant indexs to prevent unknown number in the code
-PRODUCT_INDEX = 0  # Index in the data list of the sale post
-PRICE_INDEX = 1  # Index in the data list of the sale post
-LOCATION_INDEX = 2  # Index in the data list of the sale post
 TOTAL_PERCENTAGE = 100  # Represent the total percentage of discount calculations
 
 
 # Sale post implements post, represent post of sale with product description, price and location
 # can discount and update the state of the product (for sale or sold)
 class PostSale(Post):
-
-    __for_sale_ = None  # state of the product
+    __product = None  # product description
+    __price = None  # product price
+    __location = None  # product location
+    __for_sale_ = None  # status of the product
 
     # Constructor for sale post, changing the _post_data and adding for sale, for product status
-    def __init__(self, author, *args):
-        try:    # Validation of proper arguments for sale post
-            if len(args[PRODUCT_INDEX]) < 1 or args[PRICE_INDEX] < 1 or len(args[LOCATION_INDEX]) < 1:
-                raise SalePostCreationError
-            super().__init__(author=author, data=None)  # Initial data to none and overwrite it manually
-            self._post_data: list = [args[PRODUCT_INDEX], args[PRICE_INDEX], args[LOCATION_INDEX]]  # data overwrite
-            self.__for_sale = "For sale!"  # Adding product status
-            print(self)  # Printing post after creation
-        except (SalePostCreationError, Exception) as e:
-            print(e)
+    def __init__(self, author, product, price, location):
+        super().__init__(author=author)  # Initial data to none and overwrite it manually
+        self.__product = str(product)
+        self.__price = price
+        self.__location = str(location)
+        self.__for_sale = "For sale!"  # Adding product status
+        print(self)  # Printing post after creation
 
     # Special method being overridden, to print the data as required.
     def __str__(self):
-        return (f'{self._author.username} posted a product for sale:\n{self.__for_sale} {self._post_data[PRODUCT_INDEX]},'
-                f' price: {self._post_data[PRICE_INDEX]}, pickup from: {self._post_data[LOCATION_INDEX]}\n')
+        return (f'{self._author.username} posted a product for sale:\n{self.__for_sale} {self.__product},'
+                f' price: {self.__price}, pickup from: {self.__location}\n')
 
     # Decrease the price of the product for sale
     def discount(self, discount_rate, password):
@@ -39,13 +34,13 @@ class PostSale(Post):
                 raise WrongPassword
             elif self.__for_sale == "Sold!":  # 2. Exceptions: Product Availability, Validation
                 raise ProductSoldError
-            elif not (0 < discount_rate <= TOTAL_PERCENTAGE):  # 3. Exceptions: Logic Discount (max discount 100), Validation
+            elif not (
+                    0 < discount_rate <= TOTAL_PERCENTAGE):  # 3. Exceptions: Logic Discount (max discount 100), Validation
                 raise InvalidDiscountError
-            price = self._post_data[PRICE_INDEX]  # Current Product Price
+            price = self.__price  # Current Product Price
             discount = price * (discount_rate / TOTAL_PERCENTAGE)  # Discount Price(number)
             discounted_price = price - discount  # Product Price after Discount
-            self._post_data[
-                PRICE_INDEX] = discounted_price  # Changing the product price
+            self.__price = discounted_price  # Changing the product price
             print(f"Discount on {self._author.username} product! the new price is: {discounted_price:.1f}")
         except (WrongPassword, ProductSoldError, Exception) as e:
             print(e)
